40d92be473ebb45b7c75dc7a8f7146a84af5a954
Ben Trybulski <btrybulski1@gmail.com>
2019-04-08 10:12:05 -0400

    fix

--- tr-backend/src/routes/demo.js
+++ tr-backend/src/routes/demo.js
@@ -24,48 +24,103 @@ mongoose.connect(DATABASE_URL, { useNewUrlParser: false }/* Make true for Heroku
     }
 });
 
-// Example data definition / entry //////////
+/////////////////////////////////////////////
 
-const SampleSchema = new mongoose.Schema({
-    first: String,
-    last: String,
+const RACSchema = new mongoose.Schema({
+    rid: { type: Number, required: "Must provide a unique id for each RAC" },
+    name: String,
+    activationCodes: [Number],
+    notes: String,
+});
+
+const RAC = mongoose.model("RAC", RACSchema);
+
+/////////////////////////////////////////////
+
+const ActivationSchema = new mongoose.Schema({
+    aid: { type: Number, required: "Must provide a unique id for each Activation" },
+    name: String,
+    updated: { type: Date, default: Date.now },
+    rac: Number,
+    criteria: [String],
+    notes: [String],
+});
+
+const Activation = mongoose.model("Activation", ActivationSchema);
+
+/////////////////////////////////////////////
+
+const HospitalSchema = new mongoose.Schema({
+    hid: { type: Number, required: "Must provide a unique id for each Hospital" },
+    name: { type: String, required: "Must provide a valid name for each Hospital" },
+    rac: Number,
+    address: String,
+    latitude: Number,
+    longitude: Number,
+    phoneDirectory: [{ connection: String, number: String }],
     email: String,
+    services: [String],
+    notes: [String],
 });
 
-const Sample = mongoose.model('Sample', SampleSchema);
-
-const sampleDocument = new Sample({ first: "Bob", last: "Smith", email: "bob_smith@gmail.com"});
-const sampleDocuments = [
-    {first: "Hank", last: "Jones", email: "hank_jones@gmail.com"},
-    {first: "Judy", last: "Bloom", email: "judy_bloom@gmail.com"},
-    {first: "Nelly", last: "Sanders", email: "nelly_sanders@gmail.com"},
-    {first: "Aaron", last: "Rodriguez", email: "arod@gmail.com"},
-    {first: "Billy Ray", last: "Cyrus", email: "achy_breaky@gmail.com"},
-];
-
-Sample.collection.drop(); // reset data during development
-
-sampleDocument.save((err) => {
-    if (err) {
-        console.error(err); 
-    } else {
-        console.log("sampleDocument saved to database");
-    }
-});
-
-Sample.insertMany(sampleDocuments, (err) => {
-    if (err) {
-        console.error(err); 
-    } else {
-        console.log("collection of documents saved to database");
-    }
-});
+const Hospital = mongoose.model("Hospital", HospitalSchema);
 
 /////////////////////////////////////////////
 
 function populateDatabase() {
-    // If data exists, exit
+    RAC.collection.drop();
+    Activation.collection.drop();
+    Hospital.collection.drop();
+
+    const RACData = [
+        { rid: 0, name: "Mid-Carolina", activationCodes: [0,1,2,3], notes: " "},
+    ];
+
+    const ActivationData = [
+        { aid: 0, name: "Adult Red", rac: 0, criteria: ["leg broke", "no hands"], notes: [""]},
+        { aid: 1, name: "Adult Yellow", rac: 0, criteria: ["arm broke", "no legs"], notes: [""]},
+    ];
+
+    const HospitalData = [
+        { hid: 0, name: "UNC Health Care", rac: 0, latitude: 0, longitude: 0,
+          address: "101 Manning Drive, N.C. Neurosciences Hospital, Basement, Chapel Hill, NC 27514",
+          phoneDirectory: [
+              { connection: "Patient Logistics Center-1", number: "1-855-PLC-4-UNC" },
+              { connection: "Emergency Department-Charge Nurse", number: "984-974-5602" },
+              { connection: "BAT PhONE", number: "984-974-2024" }
+          ],
+          email: "tarheeltrauma@unchealth.unc.edu",
+          services: [""],
+          notes: ["GOAL: Door to Decision Time 15 Minutes..."]
+        },
+    ];
+
+    RAC.insertMany(RACData, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("RACs inserted in database");
+        }
+    });
+
+    Activation.insertMany(ActivationData, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Activations inserted in database");
+        }
+    });
+
+    Hospital.insertMany(HospitalData, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Hospitals inserted in database");
+        }
+    });
 }
+
+populateDatabase();
 
 /////////////////////////////////////////////
 
@@ -93,19 +148,23 @@ app.get("/", (req, res) => {
     res.send('requests are working\n');
 });
 
+app.get("/directory", (req, res) => {
+    
+    
+});
+
 /////////////////////////////////////////////
 // Sample route with db querying
-
-app.get("/sample", (req, res) => {
-    Sample.find({}, (err, docs) => {
-     if (err) {
-         console.error(err);
-         res.send("Error retrieving samples");
-     } else {
-         res.send(JSON.stringify(docs));
-     }
-    });
-});
+// app.get("/sample", (req, res) => {
+//     Sample.find({}, (err, docs) => {
+//      if (err) {
+//          console.error(err);
+//          res.send("Error retrieving samples");
+//      } else {
+//          res.send(JSON.stringify(docs));
+//      }
+//     });
+// });
 
 app.listen(PORT, (err) => {
     if (err) {
--- tr-backend/src/routes/hospital.js
+++ tr-backend/src/routes/hospital.js
@@ -6,33 +6,33 @@ const Hospital = mongoose.model('Hospital');
 
 router.get('/', (req, res) => {
     res.render('hospital/addOrEdit', {
-	viewTitle : "Add Hospital"
+        viewTitle : "Add Hospital"
     });
 });
 
 router.post('/', (req, res) => {
-	if(!req.body) {
-		return res.status(400).send('Request body is missing');
-	}
-	let newHospital = new Hospital(req.body);
-	newHospital.save()
-		.then(doc => {
-			if(!doc || doc.length === 0) {
-				return res.status(500).send(doc);
-			}
+    if (!req.body) {
+        return res.status(400).send('Request body is missing');
+    }
+    let newHospital = new Hospital(req.body);
+    newHospital.save()
+        .then(doc => {
+            if(!doc || doc.length === 0) {
+                return res.status(500).send(doc);
+            }
 
-			res.status(201).send(doc);
-		})
-		.catch(err => {
-			res.status(500).json(err);
-		});
+            res.status(201).send(doc);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 // router.post('/', (req, res) => {
 //     if (req.body._id == '') {
-// 	insertRecord(req, res);
+//         insertRecord(req, res);
 //     } else {
-// 	updateRecord(req, res);
+//         updateRecord(req, res);
 //     }
 // });
 
@@ -45,103 +45,101 @@ function insertRecord(req, res) {
     hospital.address = req.body.address;
     hospital.traumaLevel = req.body.traumaLevel;
     hospital.save((err, doc) => {
-	if (!err) {
-	    res.redirect('hospital/list');
-	} else {
-	    if (err.name == 'ValidationError') {
-		handleValidationError(err, req.body);
-		res.render('hospital/addOrEdit', {
-		    viewTitle : "Add Hospital",
-		    hospital : req.body
-		});
-	    } else {
-		console.log(`Error during record insertion : ${err}`);
-	    }
-	}
+        if (!err) {
+            res.redirect('hospital/list');
+        } else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render('hospital/addOrEdit', {
+                    viewTitle : "Add Hospital",
+                    hospital : req.body
+                });
+            } else {
+                console.log(`Error during record insertion : ${err}`);
+            }
+        }
     });
 }
 
 function updateRecord(req, res) {
     Hospital.findOneAndUpdate({ _id: req.body._id}, req.body, {new: true}, (err, doc) => {
-	if (!err) {
-	    res.redirect('hospital/list');
-	} else {
-	    if (err.name == 'ValidationError') {
-		handleValidationError(err, req.body);
-		res.render("hospital/addOrEdit", {
-		    viewTitle: 'Update Hospital',
-		    hospital: req.body
-		});
-	    } else {
-		console.log(`Error during record update : ${err}`);
-	    }
-	}
+        if (!err) {
+            res.redirect('hospital/list');
+        } else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("hospital/addOrEdit", {
+                    viewTitle: 'Update Hospital',
+                    hospital: req.body
+                });
+            } else {
+                console.log(`Error during record update : ${err}`);
+            }
+        }
     });
 }
 
 function handleValidationError(err, body) {
     for (field in err.errors) {
-	switch(err.errors[field].path) {
-	case 'hospitalName':
+        switch(err.errors[field].path) {
+        case 'hospitalName':
             body['hospitalNameError'] = err.errors[field].message;
             break;
-	case 'email':
+        case 'email':
             body['emailError'] = err.errors[field].message;
             break;
-	default:
+        default:
             break;
-	}
+        }
     }
 };
 
 router.get('/list', (req, res) => {
     Hospital.find((err, docs) => {
-	if (!err) {
-	    res.render('hospital/list', {
-		list: docs
-	    });
-	} else {
-	    console.log(`Error in retrieving hospital list : ${err}`);
-	}
+        if (!err) {
+            res.render('hospital/list', {
+                list: docs
+            });
+        } else {
+            console.log(`Error in retrieving hospital list : ${err}`);
+        }
     });
 });
 
 router.get('/index', (req, res) => {
-  Hospital.find((err, docs) => {
-    if (!err) {
-      res.render('hospital/index', {
-        list: docs
-      });
-      console.log(docs);
-    } else {
-      console.log(`Error in retrieving hospital index : ${err}`);
-    }
-  });
+    Hospital.find((err, docs) => {
+        if (!err) {
+            res.render('hospital/index', {
+                list: docs
+            });
+            console.log(docs);
+        } else {
+            console.log(`Error in retrieving hospital index : ${err}`);
+        }
+    });
 });
 
 router.get('/:hid', (req, res) => {
-	if(!req.params.hid) {
-		return res.status(400).send('Missing URL parameter: hospital id (hid)')
-	}
-	Hospital.findOne({
-		hid: req.params.hid
+    if (!req.params.hid) {
+        return res.status(400).send('Missing URL parameter: hospital id (hid)')
+    }
+    Hospital.findOne({ hid: req.params.hid })
+	.then(doc => {
+            res.json(doc);
 	})
-		.then(doc => {
-			res.json(doc);
-		})
-		.catch(err => {
-			res.status(404).json(err);
-		});
+	.catch(err => {
+            res.status(404).json(err);
+        });
 });
 
 // router.get('/:id', (req, res) => {
 //     Hospital.findById(req.params.id, (err, doc) => {
-// 	if (!err) {
-// 	    res.render('hospital/addOrEdit', {
-// 		viewTitle: 'Update Hospital',
-// 		hospital: doc
-// 	    });
-// 	}
+//  if (!err) {
+//      res.render('hospital/addOrEdit', {
+//    viewTitle: 'Update Hospital',
+//    hospital: doc
+//      });
+//  }
 //     });
 // });
 
