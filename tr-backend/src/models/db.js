b51da3eb30a6a973d2210aba1eb2a7381d8c2709
PrzemekPudelko <ppudelko@live.unc.edu>
2019-04-10 14:28:55 -0400

    moved code around in db.js

--- tr-backend/src/models/db.js
+++ tr-backend/src/models/db.js
@@ -2,19 +2,6 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const path = require('path');
 
-// Read credentials from localfile
-const credsFile = path.resolve(__dirname, 'credentials.txt');
-const contents = fs.readFileSync(credsFile).toString().split(";");
-
-let sp = contents[0].split(" = ")[1];
-let dp = contents[1].split(" = ")[1];
-let up = contents[2].split(" = ")[1];
-let pp = contents[3].split(" = ")[1];
-
-let server = sp.substring(1, sp.length-1);
-let database = dp.substring(1, dp.length-1);
-let user = up.substring(1, up.length-1);
-let password = pp.substring(1, pp.length-1);
 
 let uri = process.env.MONGODB_URI;
 if (uri) {
@@ -28,6 +15,19 @@ if (uri) {
         }
 		);
 } else {
+    // Read credentials from localfile
+    const credsFile = path.resolve(__dirname, 'credentials.txt');
+    const contents = fs.readFileSync(credsFile).toString().split(";");
+
+    let sp = contents[0].split(" = ")[1];
+    let dp = contents[1].split(" = ")[1];
+    let up = contents[2].split(" = ")[1];
+    let pp = contents[3].split(" = ")[1];
+
+    let server = sp.substring(1, sp.length-1);
+    let database = dp.substring(1, dp.length-1);
+    let user = up.substring(1, up.length-1);
+    let password = pp.substring(1, pp.length-1);
     mongoose.connect(`mongodb://${user}:${password}@${server}/${database}`, {useNewUrlParser: true},
         (err) => {
             if (!err) {
