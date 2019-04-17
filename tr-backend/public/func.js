489af2c61e8f7d263dab5db876d07be8cf51fb93
andrewleift <46970738+andrewleift@users.noreply.github.com>
2019-04-17 17:09:36 -0400

    Connected Edit/Save Toggle buttons.
    laid down groundwork for delete and create new entries.
    
    All of this is across the general js file and written generically. Call distinction is in the specific hbs file.
    
    All of thses changes are only visible in the Activations page ATM as I have notupdated the hbs pages for the rest yet

--- tr-backend/public/func.js
+++ tr-backend/public/func.js
@@ -32,7 +32,8 @@ function generateArrayTable(td){
 function toggleField(tag){
   if($(tag).hasClass("mutable")){
     if($(tag).find(".field").length != 0){
-
+      let content = $(tag).find("textarea:lt(1)").val();
+      $(tag).html(content);
     }else{
       let content = $(tag).html();
       //console.log($(tag) + " :: " + content);
@@ -41,14 +42,26 @@ function toggleField(tag){
   }
 }
 
+function saveRow(tr, type){
+  let temp = $("tbody").children().index(tr);
+  tr.find(".fa-save").css({"display":"none"});
+  tr.find(".fa-pencil").css({display:"inherit"});
+  updateRequest("/activations/"+tr.children("td:nth-child(2)").text(), createJsonBody(type,temp));
+  $.each(tr.children(),function(index,val){
+    toggleField(val);
+  });
+  index = null;
+}
+
 function editRow(tr){
   let temp = $("tbody").children().index(tr);
-
   if(index == null){
     index = temp;
     $.each(tr.children(),function(index,val){
       toggleField(val);
     });
+    tr.find(".fa-save").css({display:"inherit"});
+    tr.find(".fa-pencil").css({"display":"none"});
   }else{
     console.log("You must first finish editing the open row");
   }
@@ -59,31 +72,64 @@ function deleteRow(tr){
   console.log("are you sure you want to delete the row at index" + temp + "?");
 }
 
-function createJsonBody(dict){
+function createJsonBody(dict,row){
   if(index != null){
     let temp = {};
     let i = 0;
-    $.each($("tbody tr:nth-child("+(index+1)+")").children(),function(){
-      if( $(this).children("div").length > 0 ){ // then we parse the array
-        let dataArray = [];
-        $.each($(this).find("ul").children(), function(){
-          dataArray.push($(this).text());
-        });
-        temp[dict[i]] = dataArray;
-      }else{
-        temp[dict[i]] = $(this).children("textarea:lt(1)").val();
+    $.each($("tbody tr:nth-child("+(row+1)+")").children(),function(){
+      if($(this).children("button").length == 0){ // to make sure we account for the first row of buttons
+        if( $(this).children("div").length > 0 ){ // then we parse the array
+          let dataArray = [];
+          $.each($(this).find("ul").children(), function(){
+            dataArray.push($(this).text());
+          });
+          temp[dict[i]] = dataArray;
+        }else{
+          temp[dict[i]] = $(this).children("textarea:lt(1)").val();
+        }
+        i++;
       }
-      i++;
     });
     return JSON.stringify(temp);
   }
 }
 
-function updateRow(url,data){ // PUT
-  console.log(data);
+function createRequest(url,data){
+  $.ajax(url, {
+	 async: true,
+	 type: 'POST',
+   contentType: 'application/json',
+	 dataType: 'json',
+	 data: data,
+	 xhrFields: {withCredentials: true},
+	 success: function(response) {
+		  // successful create of itinerary
+      console.log(response);
+	 },
+	 error: function(err) { console.log("Sum ting wong"); console.log(err); }
+  });
+}
+
+function updateRequest(url,data){ // PUT
   $.ajax(url, {
 	 async: true,
 	 type: 'PUT',
+   contentType: 'application/json',
+	 dataType: 'json',
+	 data: data,
+	 xhrFields: {withCredentials: true},
+	 success: function(response) {
+		  // successful create of itinerary
+      console.log(response);
+	 },
+	 error: function(err) { console.log("Sum ting wong"); console.log(err); }
+  });
+}
+
+function deleteRequest(url){ // PUT
+  $.ajax(url, {
+	 async: true,
+	 type: 'DELETE',
    contentType: 'application/json',
 	 dataType: 'json',
 	 data: data,
